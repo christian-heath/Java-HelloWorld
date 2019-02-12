82c131644a453c9f582beaf1376c2844260d1415
Christian <chelseah3@icloud.com>
2019-02-12 12:01:59 -0600

    Console lines

---
+++ ConsoleStuff.java
@@ -0,0 +1,7 @@
+public class ConsoleStuff {
+    public static void main(String[] args) {
+        System.out.println("My name is Oklahoma");
+        System.out.println("I am 112 years old.");
+        System.out.println("The unoffical state motto is 'Oklahoma - In God We Trust!'");
+    }
+}
